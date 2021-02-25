4573fb1bc622edbd6882d7663710a6b907cc3f40
Cho seokgyu <ezaurum@gmail.com>
2021-02-25 11:06:19 +0900

    Update 2021-02-25.js

--- 2021-02-25.js
+++ 2021-02-25.js
@@ -1,26 +1,29 @@
 //https://programmers.co.kr/learn/courses/30/lessons/42576
 
 function solution(participant, completion) {
-    var m = completion.reduce( function (acc, cur) {
-        acc[cur] = (acc[cur] || 0) + 1
+    const m = completion.reduce( (acc, cur) => {
+        acc[cur] = (acc[cur] | 0) + 1
         return acc
     }, {});
-
-    var answer = '';
-    var answerCount = 0;
-
-    const l = participant.length || 0;
+    
+    if (!m) {
+        return ""
+    }
+    
+    let answer = '';
+    let answerCount = 0;
+    
+    const l = participant.length | 0;
     for (let i = 0; i < l; i++ ) {
        answer = participant[i];
-       answerCount = m[answer] || 0;
-
+       answerCount = m[answer] | 0;
+        
         if (answerCount > 0) {
             m[answer] = answerCount -1;
             continue;
         }
         return answer;    
     }
-
+    
      return answer;
 }
-
